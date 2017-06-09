f7bbd9cce3d7d891445a47180d2e1052dfe50580
Jacob Castello <jacobcastello@gmail.com>
2017-06-09 00:47:53 -0400

    just testing basic init

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,33 @@
 require 'spec_helper'
 describe 'taskwarrior_server' do
   context 'with default values for all parameters' do
-    it { should contain_class('taskwarrior_server') }
+    let(:facts){
+      { 
+      :fqdn => 'localhost.local',
+      :osfamily => 'RedHat',
+      :os => { 
+        :architecture => 'x86_64', 
+        :family => 'RedHat', 
+        :hardware => 'x86_64', 
+        :name => 'CentOS', 
+        :release => { 
+           :full => '7.2.1511',
+           :major => '7',
+           :minor => '2' 
+        } 
+      }
+    }
+  }
+
+    it { is_expected.to compile }
+    it { is_expected.to contain_class('taskwarrior_server') }
+    it do
+      is_expected.to contain_file('/var/taskd/config').with({
+        'ensure'  => 'file',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+      })
+    end
   end
 end
